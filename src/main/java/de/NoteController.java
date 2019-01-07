b532416c737eaa7e9d009190fecfef42e1ccc691
lutz <lutzschulmail@web.de>
2019-01-07 16:32:10 +0100

    jenkinsfile update8

--- src/main/java/de/NoteController.java
+++ src/main/java/de/NoteController.java
@@ -14,13 +14,15 @@ import javax.xml.bind.Unmarshaller;
 public class NoteController {
 
     static Notes notes = new Notes();
+    static String path = "notes.xml";
+    static String testPath ="notes-test.xml";
 
     public static void addNote(Note note) {
-        NoteController.notes.addNote(note);
+        notes.addNote(note);
     }
 
     public static void deleteNote(Note note) {
-        NoteController.notes.deleteNote(note);
+        notes.deleteNote(note);
     }
 
     public static void loadNotes() {
@@ -28,7 +30,7 @@ public class NoteController {
             JAXBContext jaxbContext = JAXBContext.newInstance(Notes.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
-            NoteController.notes = (Notes) jaxbUnmarshaller.unmarshal(new File("notes.xml"));
+            notes = (Notes) jaxbUnmarshaller.unmarshal(new File(testPath));
         } catch (Exception e) {
             System.out.println("There was an error with loading Notes: " + e);
         }
@@ -40,7 +42,7 @@ public class NoteController {
             JAXBContext context = JAXBContext.newInstance(notes.getClass());
             Marshaller marshaller = context.createMarshaller();
             // I want to save the output file to item.xml
-            marshaller.marshal(notes, new FileWriter("notes.xml"));
+            marshaller.marshal(notes, new FileWriter(testPath));
         } catch (Exception e) {
             System.out.println("There was the following Exception: " + e);
         }
